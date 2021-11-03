116b0494c329667973499fd795a2d8384c48fb57
malickGB <malick.grandbois@gmail.com>
2021-11-03 21:52:16 +0100

    tests

--- tests/expense.py
+++ tests/expense.py
@@ -1,8 +1,10 @@
 import unittest
 from expense import new_expense
+from user import add_user
 
 class TestExpense(unittest.TestCase):
 
     def test_add_expense(self):
         self.assertEqual(new_expense(23, 'toto', 'pchojka'), True)
         self.assertEqual(new_expense('bonjour',23,23), False)
+        self.assertEqual(add_user('malick'), True)
